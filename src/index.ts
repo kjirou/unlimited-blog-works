0abc71c01e7ed82ac87e4e66749be26382a08774
kjirou <sorenariblog@gmail.com>
2019-01-28 23:16:37 +0900

    Note the most recent tasks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export function executeInit(destinationPath: string): string {
+  // TODO
+  // 1) ディレクトリありますか？
+  // 2) ディレクトリがないなら生成を試みる
+  // 3) 生成できなかったらエラーで落とす
+  // 4) src/index.md を生成する
   return destinationPath;
 }
