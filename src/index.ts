0ee333de2f417d0d7dddaaf87355f630503b27ec
kjirou <sorenariblog@gmail.com>
2019-01-30 23:12:14 +0900

    Change the data source to articles only

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import * as path from 'path';
 const CONFIGS_FILE_NAME: string = 'ubwconfigs.json';
 const RELATIVE_SRC_DIR_PATH: string = 'src';
 
+const RELATIVE_ARTICLE_MARKDOWNS_DIR_PATH: string = 'articles';
+
 export function executeInit(destinationDirPath: string): string {
   fs.ensureDirSync(destinationDirPath);
 
@@ -11,7 +13,7 @@ export function executeInit(destinationDirPath: string): string {
   fs.ensureDirSync(srcDirPath);
 
   fs.writeFileSync(
-    path.join(destinationDirPath, CONFIGS_FILE_NAME),
+    path.join(srcDirPath, CONFIGS_FILE_NAME),
     JSON.stringify(
       {
         blogName: 'Your blog',
@@ -21,10 +23,13 @@ export function executeInit(destinationDirPath: string): string {
     ) + '\n'
   );
 
+  const articleMarkdownsDirPath = path.join(srcDirPath, RELATIVE_ARTICLE_MARKDOWNS_DIR_PATH);
+  fs.ensureDirSync(articleMarkdownsDirPath);
+
   fs.writeFileSync(
-    path.join(srcDirPath, 'index.md'),
+    path.join(articleMarkdownsDirPath, '00000001.md'),
     [
-      '# Your blog',
+      '# My First Article\n',
     ].join('\n')
   );
 
