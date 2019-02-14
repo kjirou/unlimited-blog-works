c0968693df20a9e0698b1fa4a040c6093aa12540
kjirou <sorenariblog@gmail.com>
2019-02-14 22:28:01 +0900

    Update an example

--- examples/playground/ubw-configs.js
+++ examples/playground/ubw-configs.js
@@ -2,6 +2,7 @@ module.exports = {
   "blogName": "My Blog",
   "blogPath": ".",
   "publicationPath": "./docs",
+  "baseUrl": "/",
   "cssUrl": "/external-resources/index.css",
   "jsUrl": "/external-resources/index.js",
   "language": "en",
