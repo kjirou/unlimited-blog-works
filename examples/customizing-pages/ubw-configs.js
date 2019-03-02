d8ed9ce15d7ab97f79296e87b7e9651e4921dfc7
kjirou <sorenariblog@gmail.com>
2019-03-02 16:57:30 +0900

    Update an example

--- examples/customizing-pages/ubw-configs.js
+++ examples/customizing-pages/ubw-configs.js
@@ -11,14 +11,16 @@ const nonArticles = [
   {
     nonArticlePageId: 'my-top',
     url: 'index.html',
+    useLayout: true,
     render(props) {
       return `<h1><a href="/author.html">Author's Blog</a></h1>
-<ul>${props.articles.map(a => `<li><a href="${a.permalink}">${a.pageTitle}</a></li>`)}</ul>`;
+<ul>${props.articles.map(a => `<li><a href="${a.rootRelativePath}">${a.pageTitle}</a></li>`)}</ul>`;
     },
   },
   {
     nonArticlePageId: 'author',
     url: 'author.html',
+    useLayout: true,
     render(props) {
       return '<p>My name is Foo</p>';
     },
